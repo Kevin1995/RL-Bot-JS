bb4fac133b2b8e8f9cbe21dd5b742e65a9179c46
Kevin Kehoe <kevinkehoe18@gmail.com>
2021-11-09 22:40:01 +0000

    Queue (WIP2) Optional Options & Verifying checks based on playlist for each player

--- commands/queue.ts
+++ commands/queue.ts
@@ -16,6 +16,10 @@ export default {
             type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
             choices: [
                 {
+                    name: "1s",
+                    value: "1s"
+                },
+                {
                     name: "2s",
                     value: "2s"
                 },
@@ -42,16 +46,43 @@ export default {
             ]
         },
         {
-            name: 'playerTwo',
+            name: 'playertwo',
             description: 'Tag the 2nd player that will be playing with you.',
-            required: true,
+            required: false,
             type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
         },
         {
-            name: 'playerThree',
+            name: 'playerthree',
             description: 'Tag the 3rd player that will be playing with you.',
             required: false,
             type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
         }
     ],
-}
+
+    callback: async ({ interaction }) => {
+        const { options } = interaction
+        const discordID = interaction.user.id
+        const playlist = options.getString('playlist')!
+        let playerTwo = options.getString('playertwo')!
+        let playerThree = options.getString('playerthree')!
+
+        if (playlist === '1s' && playerTwo == null && playerThree == null) {
+            console.log(`${playlist} game is all sorted`)
+            console.log(discordID)
+        }
+        else if ((playlist === '2s' || playlist === 'Hoops') && playerTwo !== null && playerThree == null) {
+            console.log(`${playlist} game is all sorted`)
+            playerTwo = playerTwo.replace(/[<@!&>]/g, '')
+            console.log(discordID, playerTwo)
+        }
+        else if ((playlist !== '1s' && playlist !== '2s' && playlist !== 'Hoops') && playerTwo !== null && playerThree !== null) {
+            console.log(`${playlist} game is all sorted`)
+            playerTwo = playerTwo.replace(/[<@!&>]/g, '')
+            playerThree = playerThree.replace(/[<@!&>]/g, '')
+            console.log(discordID, playerTwo, playerThree)
+        } else {
+            console.log(`You have not used the command correctly for a ${playlist} game.`)
+            return
+        }
+    }
+} as ICommand
